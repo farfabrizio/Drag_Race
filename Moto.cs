fd131c82711c5e4a0bb73c6e88c0811be7c5ea3e
Fabrizio Faraci <fabrizio.faraci@itis.biella.it>
2026-09-22 16:15:23 +0200

    Aggiungere i file di progetto.

---
+++ Moto.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drag_Race
+{
+    internal class Moto
+    {
+        private string _marca;
+        private string _modello;
+        private int _cavalli;
+
+        private double _tempoZeroCento;
+        private int _cilindrata;
+
+
+
+        //definiamo le proprietà della classe Moto
+        public string Marca
+        {
+            get { return _marca; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("La marca non può essere vuota.");
+                }
+                else
+                {
+                    _marca = value;
+                }
+            }
+        }
+
+        public string Modello
+        {
+            get { return _modello; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Il modello non può essere vuoto.");
+                }
+                else
+                {
+                    _modello = value;
+                }
+            }
+        }
+        public double TempoZeroCento
+        {
+            get { return _tempoZeroCento; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Il tempo 0-100 non può essere negativo.");
+                }
+                else
+                {
+                    _tempoZeroCento = value;
+                }
+            }
+        }
+
+        public int Cavalli
+        {
+            get { return _cavalli; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("I cavalli non possono essere negativi.");
+                }
+                else
+                {
+                    _cavalli = value;
+                }
+            }
+        }
+
+        public int Cilindrata
+        {
+            get { return _cilindrata; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("La cilindrata non può essere negativa.");
+                }
+                else
+                {
+                    _cilindrata = value;
+                }
+            }
+        }
+
+        //definiamo il costruttore della classe Moto
+        public Moto(string marca, string modello, int cavalli, double tempoZeroCento, int cilindrata)
+        {
+            Marca = marca;
+            Modello = modello;
+            Cavalli = cavalli;
+            TempoZeroCento = tempoZeroCento;
+            Cilindrata = cilindrata;
+
+
+        }
+
+        // override del metodo ToString()
+        public override string ToString()
+        {
+            return $"Marca: {Marca}, " +
+                $"Modello: {Modello}, " +
+                $"Cavalli: {Cavalli}, " +
+                $"Tempo 0-100: {TempoZeroCento}," +
+                $" Cilindrata: {Cilindrata}";
+        }
+
+        //metodo di confronto della cilindrata tra due moto
+        public string ConfrontoCilindrata(Moto moto)
+        {
+            if (this.Cilindrata > moto.Cilindrata)
+            {
+                return $"{this.Marca} {this.Modello} ha una cilindrata maggiore di {moto.Marca} {moto.Modello}.";
+            }
+            else if (this.Cilindrata < moto.Cilindrata)
+            {
+                return $"{this.Marca} {this.Modello} ha una cilindrata minore di {moto.Marca} {moto.Modello}.";
+            }
+            else
+            {
+                return $"{this.Marca} {this.Modello} e {moto.Marca} {moto.Modello} hanno la stessa cilindrata.";
+            }
+
+        }
+
+        //metodo confronto accelerazione sui 100 metri
+        public string ConfrontaAccelerazione(Moto moto) {
+
+            if (this.TempoZeroCento > moto.TempoZeroCento)
+            {
+                return $"{this.Marca} {this.Modello} ha un'accelerazione più lenta di {moto.Marca} {moto.Modello}.";
+            }
+            else if (this.TempoZeroCento == moto.TempoZeroCento)
+            {
+                return $"{this.Marca} {this.Modello} e {moto.Marca} {moto.Modello} hanno la stessa accelerazione.";
+            }
+            else { 
+                return $"{this.Marca} {this.Modello} ha un'accelerazione più veloce di {moto.Marca} {moto.Modello}.";
+            }
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Drag_Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Moto Kawasaki1 = new Moto("Kawasaki","Ninja ZX-10R", 200, 2.6, 998);
+            Console.WriteLine(Kawasaki1.ToString());
+
+            Moto Yamaha1= new Moto("Yamaha", "YZF-R1", 200, 2.7, 998);
+            Console.WriteLine(Yamaha1.ToString());
+            Console.WriteLine(Kawasaki1.ConfrontoCilindrata(Yamaha1));
+
+            Console.WriteLine(Yamaha1.ConfrontaAccelerazione(Kawasaki1));
+        }
+    }
+}
